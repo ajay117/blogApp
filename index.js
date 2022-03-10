d57612dbd9ad237bec3d68d32cc42e9d3322060a
Ajay KC <meajay64@gmail.com>
2022-03-10 11:09:28 +0900

    Initialized new app with necessary file

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.port || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+let blogData = [
+  {
+    title: "Picture1",
+    image:
+      "https://images.unsplash.com/photo-1646673974332-804492240491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834&q=80",
+    description: "Some description",
+  },
+  {
+    title: "Picture2",
+    image:
+      "https://images.unsplash.com/photo-1646673974332-804492240491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834&q=80",
+    description: "Some description",
+  },
+  {
+    title: "Picture3",
+    image:
+      "https://images.unsplash.com/photo-1646673974332-804492240491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834&q=80",
+    description: "Some description",
+  },
+];
+
+// Go to Main Page...
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//Go to see all blogs...
+app.get("/blogs", (req, res) => {
+  res.render("blogs/index", { blogData });
+});
+
+app.listen(PORT, () => "Server Started");
